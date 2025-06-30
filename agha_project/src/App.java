334c20618fb9a49623ab7469cb8d5b95c0ac97af
l0agha0aghayev0l <o.agha.aghayev.o@gmail.com>
2025-06-30 15:35:32 +0400

    Updated

--- agha_project/src/App.java
+++ agha_project/src/App.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        method2();
+    }
+
+    public static void method1() {
         int[] nums = new int[10];
 
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = (int)(Math.random()*100);
+            nums[i] = (int) (Math.random() * 100);
         }
 
         List<Integer> evens = new ArrayList<>();
@@ -24,5 +28,25 @@ public class App {
         System.out.println(evens.addAll(odds));
 
         System.out.println(Arrays.toString(nums));
+    }
+
+    public static void method2() {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            nums.add((int) (Math.random() * 100));
+        }
+        List<Integer> updatedNums = new ArrayList<>();
+
+        System.out.println("Default List: " + nums);
+
+        for (Integer i : nums) {
+            if (i % 2 == 0) {
+                updatedNums.add(0, i);
+            } else {
+                updatedNums.add(updatedNums.size(), i);
+            }
+        }
+
+        System.out.println("Updated List: " + updatedNums);
     }
 }
